d21e9e25d5d1e0f2fa8da577bb2e20c538cdcacb
ilias <ilia.storozhev@yandex.ru>
2023-01-16 17:49:04 +0300

    Выполнил Hw6

--- src/Main.java
+++ src/Main.java
@@ -62,7 +62,6 @@ public class Main {
 
     public static void task7() {
         System.out.println("Задание 7");
-
         for (int i = 1; i <= 512; i = i * 2) {
             System.out.println(i);
         }
